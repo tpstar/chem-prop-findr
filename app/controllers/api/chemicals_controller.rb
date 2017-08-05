d5ef2c567178cb8ed817009da1036d450e9bc04f
tpstar <hanlee12@gmail.com>
2017-08-05 18:51:03 -0500

    add heroku web address to cors.rb

--- app/controllers/api/chemicals_controller.rb
+++ app/controllers/api/chemicals_controller.rb
@@ -1,52 +1,34 @@
 class Api::ChemicalsController < ApplicationController
-  before_action :find_chemical, only: [:show, :update, :destroy]
+
   def index
     chemicals = Chemical.all
     render json: chemicals, status: 201
   end
-  #
-  # def create
-  #   chemical = current_user.chemicals.build(chemical_params)
-  #   if chemical.save
-  #     render json: chemical
-  #   else
-  #     render json: {error: "This chemical was not created.", status: 500}, status: 500
-  #   end
-  # end
-  #
-  # def update
-  #   @chemical.update(chemical_params)
-  #   if @chemical.save
-  #     render json: @chemical
-  #   else
-  #     render json: {error: "This chemical was not updated.", status: 500}, status: 500
-  #   end
-  # end
-  #
-  # def show
-  #   if @chemical
-  #     render json: @chemical
-  #   else
-  #     render json: {error: "Could not find the chemical!", status: 404}, status: 404
-  #   end
-  # end
-  #
-  # def destroy
-  #   @chemical.destroy
-  #   render json: {message: "This chemical was deleted", status: 200}, status: 200
-  # end
 
   def search
     chemical = Chemical.find_by(:name => params[:qchemname].capitalize)
-    # check if the chemical is in the database record
+    # check if the chemical is already in the database record
     if chemical
       render json: chemical
     # if is not in the record scrape from Aldrich website
     else
       chemical = Chemical.new # create an instance of Chemical
       chemical.scrape(params[:qchemname]) # and scrape from Aldrich website
-      if chemical.save
-        render json: chemical
+      # check if the chemical exists in Aldrich website
+      if chemical
+        # check if the chemical exists in the record by other names
+        # for example methyliodide and iodomethane are the same compound, but once it is recorded as Iodomethane
+        # using search term as "methyliodide" will keep generate a new record
+        existing_chemical = Chemical.find_by(:name => chemical.name)
+        # if it exists by other name
+        if existing_chemical
+          render json: existing_chemical
+        # if it does not exist in the record by other name
+        else
+          chemical.save
+          render json: chemical
+        end
+      # if it cannot be found in Aldrich website
       else
         render json: {error: "Could not find the chemical!", status: 404}, status: 404
       end
@@ -57,10 +39,6 @@ class Api::ChemicalsController < ApplicationController
 
   def chemical_params
     params.require(:chemical).permit(:name, :formula, :fw, :density, :mp, :bp)
-  end
-
-  def find_chemical
-    @chemical = Chemical.find_by_id(params[:id])
   end
 
 end
